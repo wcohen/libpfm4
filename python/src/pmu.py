b9f7db1e8a9e00bfa3f5c532011c691a86745a3f
William Cohen <wcohen@redhat.com>
2018-06-05 11:56:20 -0400

    Eliminate a number of pylint warnings for pmu.py.

--- python/src/pmu.py
+++ python/src/pmu.py
@@ -28,75 +28,74 @@ from perfmon import *
 def public_members(self):
     s = "{ "
     for k, v in self.__dict__.items():
-      if not k[0] == '_':
-        s += "%s : %s, " % (k, v)
+        if not k[0] == '_':
+            s += "%s : %s, " % (k, v)
     s += " }"
     return s
 
 class System:
-  # Use the os that gives us everything
-  os = PFM_OS_PERF_EVENT_EXT
+    # Use the os that gives us everything
+    os = PFM_OS_PERF_EVENT_EXT
 
-  def __init__(self):
-    self.ncpus = os.sysconf('SC_NPROCESSORS_ONLN')
-    self.pmus = []
-    for i in range(0, PFM_PMU_MAX):
-      try:
-        pmu = PMU(i)
-      except:
-        pass
-      else:
-        self.pmus.append(pmu)
+    def __init__(self):
+        self.ncpus = os.sysconf('SC_NPROCESSORS_ONLN')
+        self.pmus = []
+        for i in range(0, PFM_PMU_MAX):
+            try:
+                pmu = PMU(i)
+            except:
+                pass
+            else:
+                self.pmus.append(pmu)
 
-  def __repr__(self):
-    return public_members(self)
+    def __repr__(self):
+        return public_members(self)
 
 class Event:
-  def __init__(self, info):
-    self.info = info
-    self.__attrs = []
+    def __init__(self, info):
+        self.info = info
+        self.__attrs = []
 
-  def __repr__(self):
-    return '\n' + public_members(self)
+    def __repr__(self):
+        return '\n' + public_members(self)
 
-  def __parse_attrs(self):
-    info = self.info
-    for index in range(0, info.nattrs):
-      self.__attrs.append(pfm_get_event_attr_info(info.idx, index,
-                                                  System.os)[1])
+    def __parse_attrs(self):
+        info = self.info
+        for index in range(0, info.nattrs):
+            self.__attrs.append(pfm_get_event_attr_info(info.idx, index,
+                                                        System.os)[1])
 
-  def attrs(self):
-    if not self.__attrs:
-      self.__parse_attrs()
-    return self.__attrs
+    def attrs(self):
+        if not self.__attrs:
+            self.__parse_attrs()
+        return self.__attrs
 
 class PMU:
-  def __init__(self, i):
-    self.info = pfm_get_pmu_info(i)[1]
-    self.__events = []
+    def __init__(self, i):
+        self.info = pfm_get_pmu_info(i)[1]
+        self.__events = []
 
-  def __parse_events(self):
-    index = self.info.first_event
-    while index != -1:
-      self.__events.append(Event(pfm_get_event_info(index, System.os)[1]))
-      index = pfm_get_event_next(index)
+    def __parse_events(self):
+        index = self.info.first_event
+        while index != -1:
+            self.__events.append(Event(pfm_get_event_info(index, System.os)[1]))
+            index = pfm_get_event_next(index)
 
-  def events(self):
-    if not self.__events:
-      self.__parse_events()
-    return self.__events
+    def events(self):
+        if not self.__events:
+            self.__parse_events()
+        return self.__events
 
-  def __repr__(self):
-    return public_members(self)
+    def __repr__(self):
+        return public_members(self)
 
 if __name__ == '__main__':
-  from perfmon import *
-  s = System()
-  for pmu in s.pmus:
-    info = pmu.info
-    if info.flags.is_present:
-      print(info.name, info.size, info.nevents)
-      for e in pmu.events():
-        print(e.info.name, e.info.code)
-        for a in e.attrs():
-          print('\t\t', a.name, a.code)
+    s = System()
+    for pmu in s.pmus:
+        info = pmu.info
+        if info.flags.is_present:
+            print(info.name, info.size, info.nevents)
+            for e in pmu.events():
+                print(e.info.name, e.info.code)
+                for a in e.attrs():
+                    print('\t\t', a.name, a.code)
