icl-utk-edu/papi

commit 30adc677603b28c6d9eb311de7298fa4fea26eed

lib/pfmlib_s390x_perf_event.c: Fix perf attr.type event number for s390
The s390 Performance Measurement counter facility does not have a fixed
type number anymore. This was caused by linux kernel commits
commit 66d258c5b048 ("perf/core: Optimize perf_init_event()")
and its necessary follow on commit
commit 6a82e23f45fe ("s390/cpumf: Adjust registration of s390 PMU device drivers")

Now read out the current type number from a sysfs file named
/sys/devices/cpum_cf/type. If it does not exist there is no CPU-MF counter
facility installed or activated, which has been checked before.

Signed-off-by: Thomas Richter <tmricht@linux.ibm.com>

lib/pfmlib_s390x_perf_event.c
@@ -31,6 +31,43 @@
 #include "pfmlib_perf_event_priv.h"
 
 
+/*
+ * The s390 Performance Measurement counter facility does not have a fixed
+ * type number anymore. This was caused by linux kernel commits
+ * 66d258c5b0488 perf/core: Optimize perf_init_event()
+ * and its necessary follow on commit
+ * 6a82e23f45fe0 s390/cpumf: Adjust registration of s390 PMU device drivers
+ *
+ * Now read out the current type number from a sysfs file named
+ * /sys/devices/cpum_cf/type. If it does not exist there is no CPU-MF counter
+ * facility installed or activated.
+ *
+ * As the CPU Measurement counter facility does not change on a running
+ * system, read out the type value on first read and cache it.
+ */
+#define CPUM_CF_DEVICE_TYPE  "/sys/bus/event_source/devices/cpum_cf/type"
+static int cpum_cf_type;
+static int pfm_s390_get_perf_attr_type(void)
+{
+	size_t buflen;
+	char *buffer;
+	FILE *fp;
+
+	if (cpum_cf_type)
+		return cpum_cf_type;
+
+	fp = fopen(CPUM_CF_DEVICE_TYPE, "r");
+	if (fp == NULL)
+		return cpum_cf_type;
+	buffer = NULL;
+	if (pfmlib_getl(&buffer, &buflen, fp) != -1 &&
+	    sscanf(buffer, "%u", &cpum_cf_type) == -1)
+		cpum_cf_type = PERF_TYPE_RAW;
+	fclose(fp);
+	free(buffer);
+	return cpum_cf_type;
+}
+
 int pfm_s390x_get_perf_encoding(void *this, pfmlib_event_desc_t *e)
 {
 	pfmlib_pmu_t *pmu = this;
@@ -44,7 +81,7 @@ int pfm_s390x_get_perf_encoding(void *this, pfmlib_event_desc_t *e)
 	rc = pmu->get_event_encoding[PFM_OS_NONE](this, e);
 	if (rc == PFM_SUCCESS) {
 		/* currently use raw events only */
-		attr->type = PERF_TYPE_RAW;
+		attr->type = pfm_s390_get_perf_attr_type();
 		attr->config = e->codes[0];
 	}
 

wcohen/libpfm4

commit 3833ff527012a33131f9af2530fe1447f6984ebf

search perf attr.type event number for s390
Commit 30adc677603b ("lib/pfmlib_s390x_perf_event.c: Fix perf attr.type event number for s390")
fixes the dynamic PMU type assignment by the kernel when s390x PMU device
drivers are loaded at boot time. However s390x has several PMU device
drivers. Therefore find the correct one first and then return the type
number read out from a sysfs file. Once the PMU type number is determined,
it does not change until the next reboot. It is ok to cache it.
Also add a check if the PMU really exists and return an error if not.

Fixes: 30adc677603b ("lib/pfmlib_s390x_perf_event.c: Fix perf attr.type event number for s390")

Signed-off-by: Thomas Richter <tmricht@linux.ibm.com>

lib/pfmlib_s390x_perf_event.c
@@ -44,27 +44,61 @@
  *
  * As the CPU Measurement counter facility does not change on a running
  * system, read out the type value on first read and cache it.
+ *
+ * There are several PMUs for s390, so find the correct one first and return
+ * its PMU type value assigned at system boot time.
  */
-#define CPUM_CF_DEVICE_TYPE  "/sys/bus/event_source/devices/cpum_cf/type"
-static int cpum_cf_type;
-static int pfm_s390_get_perf_attr_type(void)
+static struct pfm_s390_perf_aptt {	/* Perf attribute PMU type table */
+	pfm_pmu_t pmutype;		/* PMU Type number */
+	const char *fname;		/* File name to read type from */
+	unsigned int value;		/* Type value, 0 --> unused */
+} pfm_s390_perf_aptt[] = {
+	{
+		.pmutype = PFM_PMU_S390X_CPUM_CF,
+		.fname = "/sys/bus/event_source/devices/cpum_cf/type"
+	},
+	{
+		.pmutype = PFM_PMU_S390X_CPUM_SF,
+		.fname = "/sys/bus/event_source/devices/cpum_sf/type"
+	},
+};
+#define S390_APTT_COUNT LIBPFM_ARRAY_SIZE(pfm_s390_perf_aptt)
+
+static int pfm_s390_get_perf_attr_type(pfm_pmu_t pmutype)
 {
+	int cpum_cf_type;
 	size_t buflen;
 	char *buffer;
 	FILE *fp;
+	size_t i;
 
-	if (cpum_cf_type)
-		return cpum_cf_type;
+	/* Find type of PMU and return known and cached value */
+	for (i = 0; i < S390_APTT_COUNT; ++i) {
+		if (pfm_s390_perf_aptt[i].pmutype == pmutype)
+			break;
+	}
 
-	fp = fopen(CPUM_CF_DEVICE_TYPE, "r");
+	if (i == S390_APTT_COUNT)
+		return PFM_ERR_NOTFOUND;
+
+	if (pfm_s390_perf_aptt[i].value)
+		return pfm_s390_perf_aptt[i].value;
+
+	/* Value unknown, read from file */
+	fp = fopen(pfm_s390_perf_aptt[i].fname, "r");
 	if (fp == NULL)
-		return cpum_cf_type;
+		return PFM_ERR_NOTFOUND;
+
 	buffer = NULL;
+
 	if (pfmlib_getl(&buffer, &buflen, fp) != -1 &&
 	    sscanf(buffer, "%u", &cpum_cf_type) == -1)
 		cpum_cf_type = PERF_TYPE_RAW;
+
 	fclose(fp);
 	free(buffer);
+
+	pfm_s390_perf_aptt[i].value = cpum_cf_type;
 	return cpum_cf_type;
 }
 
@@ -81,8 +115,12 @@ int pfm_s390x_get_perf_encoding(void *this, pfmlib_event_desc_t *e)
 	rc = pmu->get_event_encoding[PFM_OS_NONE](this, e);
 	if (rc == PFM_SUCCESS) {
 		/* currently use raw events only */
-		attr->type = pfm_s390_get_perf_attr_type();
-		attr->config = e->codes[0];
+		rc = pfm_s390_get_perf_attr_type(pmu->pmu);
+		if (rc > 0) {		/* PMU types are positive */
+			attr->type = rc;
+			attr->config = e->codes[0];
+			rc = PFM_SUCCESS;
+		}
 	}
 
 	return rc;