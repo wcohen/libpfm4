wcohen/libpfm4

commit 363825f72afde0e8cae2ecfd261a95d2bd0b3868

maintain list of active PMUs
Given that the list of PMUs supported per architecture keeps growing,
it is becoming expensive to iterate over each PMU looking for a
match. The macro pfmlib_for_each_pmu() is iterating over active and
inactive PMUs looking for active ones. Given that the number of inactive
PMUs is always larger than the number of active, this was expensive.

Fix this by creating a list of active PMUs and adding a new macro
pfmlib_for_each_active_pmu(). We use a oubly linked list to allow
further optimizations.

As an example on a X86 build, the new iterator allowed a 10x reduction
in iterations inside pfmlib_parse_event() for a core PMU event.

When LIBPFM_ENCODE_INACTIVE is set to 1, then all PMUs supported by
the architecture are put on the active list even when they are not
detected. This simplifies the parsing loop.

Signed-off-by: Stephane Eranian <eranian@gmail.com>

lib/pfmlib_common.c
@@ -673,6 +673,7 @@ static pfmlib_os_t *pfmlib_oses[]={
  * architecture or if the initialization failed.
  */
 static pfmlib_pmu_t *pfmlib_pmus_map[PFM_PMU_MAX];
+static struct pfmlib_pmu *pfmlib_active_pmus_list;
 
 /*
  * A drop-in replacement for strsep(). strsep() is not part of the POSIX
@@ -710,6 +711,9 @@ static char* pfmlib_strsep(char **stringp, const char *delim)
 
 #define pfmlib_for_each_pmu(x) \
 	for((x)= 0 ; (x) < PFMLIB_NUM_PMUS; (x)++)
+
+#define pfmlib_for_each_active_pmu(x) \
+	for((x) = pfmlib_active_pmus_list; x ; (x) = (x)->next_active)
 
 #define pfmlib_for_each_os(x) \
 	for((x)= 0 ; (x) < PFMLIB_NUM_OSES; (x)++)
@@ -1115,6 +1119,16 @@ done:
 	return ret;
 }
 
+static inline void pfmlib_add_active_pmu(struct pfmlib_pmu *pmu)
+{
+	if (pfmlib_active_pmus_list)
+		pfmlib_active_pmus_list->prev_active = pmu;
+
+	pmu->next_active = pfmlib_active_pmus_list;
+	pmu->prev_active = NULL;
+	pfmlib_active_pmus_list = pmu;
+}
+
 static int
 pfmlib_init_pmus(void)
 {
@@ -1158,8 +1172,16 @@ pfmlib_init_pmus(void)
 		 */
 		pfmlib_pmus_map[p->pmu] = p;
 
-		if (ret != PFM_SUCCESS)
+		if (ret != PFM_SUCCESS) {
+			/*
+			 * if LIBPFM_ENCODE_INACTIVE=1, we place all PMUs on the active list.
+			 * we lose the optimization but we do not have to special case the parsing
+			 * code. This is a debug option anyway.
+			 */
+			if (pfm_cfg.inactive)
+				pfmlib_add_active_pmu(p);
 			continue;
+		}
 
 		/*
 		 * check if exported by OS if needed
@@ -1173,8 +1195,10 @@ pfmlib_init_pmus(void)
 		}
 
 		ret = pfmlib_pmu_activate(p);
-		if (ret == PFM_SUCCESS)
+		if (ret == PFM_SUCCESS) {
 			nsuccess++;
+			pfmlib_add_active_pmu(p);
+		}
 
 		if (pfm_cfg.forced_pmu) {
 			__pfm_vbprintf("PMU forced to %s (%s) : %s\n",
@@ -1249,15 +1273,11 @@ void
 pfm_terminate(void)
 {
 	pfmlib_pmu_t *pmu;
-	int i;
 
 	if (PFMLIB_INITIALIZED() == 0)
 		return;
 
-	pfmlib_for_each_pmu(i) {
-		pmu = pfmlib_pmus[i];
-		if (!pfmlib_pmu_active(pmu))
-			continue;
+	pfmlib_for_each_active_pmu(pmu) {
 		if (pmu->pmu_terminate)
 			pmu->pmu_terminate(pmu);
 	}
@@ -1623,7 +1643,7 @@ pfmlib_parse_event(const char *event, pfmlib_event_desc_t *d)
 	pfmlib_pmu_t *pmu;
 	int (*match)(void *this, pfmlib_event_desc_t *d, const char *e, const char *s);
 	const char *pname = NULL;
-	int i, j, ret;
+	int i, ret;
 
 	/*
 	 * support only one event at a time.
@@ -1654,15 +1674,15 @@ pfmlib_parse_event(const char *event, pfmlib_event_desc_t *d)
 	/*
 	 * for each pmu
 	 */
-	pfmlib_for_each_pmu(j) {
-		pmu = pfmlib_pmus[j];
+	pfmlib_for_each_active_pmu(pmu) {
+
 		/*
-		 * if no explicit PMU name is given, then
-		 * only look for active PMU models
+		 * test against active still required in case of
+		 * pfm_cfg.inactive=1 because we put all PMUs on
+		 * the active list to avoid forking this loop
 		 */
 		if (!pname && !pfmlib_pmu_active(pmu))
 			continue;
-
 		/*
 		 * if the PMU name is not passed, then if
 		 * the pmu is deprecated, then skip it. It means
@@ -2297,14 +2317,8 @@ pfmlib_pmu_t *
 pfmlib_get_pmu_by_type(pfm_pmu_type_t t)
 {
 	pfmlib_pmu_t *pmu;
-	int i;
 
-	pfmlib_for_each_pmu(i) {
-		pmu = pfmlib_pmus[i];
-
-		if (!pfmlib_pmu_active(pmu))
-			continue;
-
+	pfmlib_for_each_active_pmu(pmu) {
 		/* first match */
 		if (pmu->type != t)
 			continue;

lib/pfmlib_priv.h
@@ -131,6 +131,8 @@ typedef struct pfmlib_pmu {
 	const char 	*desc;			/* PMU description */
 	const char 	*name;			/* pmu short name */
 	const char	*perf_name;		/* perf_event pmu name (optional) */
+	struct pfmlib_pmu *next_active;		/* active PMU link list */
+	struct pfmlib_pmu *prev_active;		/* active PMU link list */
 	pfm_pmu_t	pmu;			/* PMU model */
 	int		pme_count;		/* number of events */
 	int		max_encoding;		/* max number of uint64_t to encode an event */

commit 9c3d167fa6017836cb6e33004471cebd4d1bf0f6

fix active PMU list handling for LIBPFM_ENCODE_INACTIVE=1
There was an issue introduced by:
 363825f72afd ("maintain list of active PMUs")

Where if a PMU is not detected because not exported by OS, it would
not be put on the active list when LIBPFM_ENCODE_INACTIVE=1 causing
tests/validate on some uncore PMU events.

Fix this by correctly handling the case where a PMU is not exported
by the OS.

Also check that a PMU is actually active in pfmlib_terminate() and
pfm_get_pmu_by_type() again to handle LIBPFM_ENCODE_INACTIVE=1.

Signed-off-by: Stephane Eranian <eranian@gmail.com>

lib/pfmlib_common.c
@@ -1210,6 +1210,12 @@ pfmlib_init_pmus(void)
 		if (p->os_detect[pfmlib_os->id]) {
 			ret = p->os_detect[pfmlib_os->id](p);
 			if (ret != PFM_SUCCESS) {
+				/*
+				 * must force on active list when
+				 * LIBPFM_ENCODE_INACTIVE=1
+				 */
+				if (pfm_cfg.inactive)
+					pfmlib_add_active_pmu(p);
 				DPRINT("%s PMU not exported by OS\n", p->name);
 				continue;
 			}
@@ -1299,6 +1305,9 @@ pfm_terminate(void)
 		return;
 
 	pfmlib_for_each_active_pmu(pmu) {
+		/* handle LIBPFM_ENCODE_INACTIVE=1 */
+		if (!pfmlib_pmu_active(pmu))
+			continue;
 		if (pmu->pmu_terminate)
 			pmu->pmu_terminate(pmu);
 	}
@@ -2346,6 +2355,9 @@ pfmlib_get_pmu_by_type(pfm_pmu_type_t t)
 	pfmlib_pmu_t *pmu;
 
 	pfmlib_for_each_active_pmu(pmu) {
+		/* handle LIBPFM_ENCODE_INACTIVE=1 */
+		if (!pfmlib_pmu_active(pmu))
+			continue;
 		/* first match */
 		if (pmu->type != t)
 			continue;