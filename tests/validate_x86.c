cdbe2eed7bdcf5d45086d6730033defc1939a722
Stephane Eranian <eranian@gmail.com>
2023-12-12 23:27:19 -0800

    Add Intel IcelakeX uncore PMU support for M3UPI
    Adds the Mesh to UPI PMU (M3UPI) for Intel IcelakeX.
    Based on Intel JSON events v1.21 published from
    github.com/intel/perfmon/ICX
    
    Signed-off-by: Stephane Eranian <eranian@gmail.com>

--- tests/validate_x86.c
+++ tests/validate_x86.c
@@ -8747,6 +8747,34 @@ static const test_event_t x86_test_events[]={
 	  .codes[0] = 0xf02,
 	  .fstr = "icx_unc_upi0::UNC_UPI_TxL_FLITS:ALL_DATA:e=0:i=0:t=0",
 	},
+	{ SRC_LINE,
+	  .name = "icx_unc_m3upi0::UNC_M3UPI_CLOCKTICKS",
+	  .ret  = PFM_SUCCESS,
+	  .count = 1,
+	  .codes[0] = 0x1,
+	  .fstr = "icx_unc_m3upi0::UNC_M3UPI_CLOCKTICKS:e=0:i=0:t=0"
+	},
+	{ SRC_LINE,
+	  .name = "icx_unc_m3upi1::UNC_M3UPI_CLOCKTICKS",
+	  .ret  = PFM_SUCCESS,
+	  .count = 1,
+	  .codes[0] = 0x1,
+	  .fstr = "icx_unc_m3upi1::UNC_M3UPI_CLOCKTICKS:e=0:i=0:t=0"
+	},
+	{ SRC_LINE,
+	  .name = "icx_unc_m3upi2::UNC_M3UPI_CLOCKTICKS",
+	  .ret  = PFM_SUCCESS,
+	  .count = 1,
+	  .codes[0] = 0x1,
+	  .fstr = "icx_unc_m3upi2::UNC_M3UPI_CLOCKTICKS:e=0:i=0:t=0"
+	},
+	{ SRC_LINE,
+	  .name = "icx_unc_m3upi3::UNC_M3UPI_CLOCKTICKS",
+	  .ret  = PFM_SUCCESS,
+	  .count = 1,
+	  .codes[0] = 0x1,
+	  .fstr = "icx_unc_m3upi3::UNC_M3UPI_CLOCKTICKS:e=0:i=0:t=0"
+	},
 };
 
 #define NUM_TEST_EVENTS (int)(sizeof(x86_test_events)/sizeof(test_event_t))
