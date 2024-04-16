wcohen/libpfm4

commit 9410619f922facca7dab2406c58fe41a8dd61529

update task.c example to handle hybrid
Cannot group event if they do not belong to the same hardware PMU.

Signed-off-by: Stephane Eranian <eranian@gmail.com>

perf_examples/task.c
@@ -250,12 +250,15 @@ parent(char **arg)
 
 	for(i=0; i < num_fds; i++) {
 		int is_group_leader; /* boolean */
+		uint32_t group_pmu = -1;
 
+		/* we can only group events if the belong to the same PMU */
 		is_group_leader = perf_is_group_leader(fds, i);
 		if (is_group_leader) {
 			/* this is the group leader */
 			group_fd = -1;
-		} else {
+			group_pmu = fds[i].hw.type;
+		} else if (fds[i].hw.type == group_pmu) { /* same PMU */
 			group_fd = fds[fds[i].group_leader].fd;
 		}
 

commit 1c0cdb91bc79eb1bc827e022f0a3738f124796a2

fix uninitialized variable in perf_examples/task.c
Commit 9410619f922f ("update task.c example to handle hybrid")

Introduced a bug by not initializing group_fd which could generate
a compiler warning and a bug.
Fix this by initializing group_fd to -1.

Reported-by: William Cohen <wcohen@redhat.com>
Signed-off-by: Stephane Eranian <eranian@gmail.com>

perf_examples/task.c
@@ -180,7 +180,7 @@ int
 parent(char **arg)
 {
 	perf_event_desc_t *fds = NULL;
-	int status, ret, i, num_fds = 0, grp, group_fd;
+	int status, ret, i, num_fds = 0, grp, group_fd = -1;
 	int ready[2], go[2];
 	char buf;
 	pid_t pid;