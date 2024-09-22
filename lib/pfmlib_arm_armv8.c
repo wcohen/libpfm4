c89a379175c00a20bbc660ad9b444e8ecc16cd28
Stephane Eranian <eranian@gmail.com>
2024-09-21 21:11:10 -0700

    add ARM Cortex A76 core PMU support
    Adds ARM Cortex A76 core PMU support.
    Based on:
    	https://github.com/ARM-software/data/blob/master/pmu/cortex-a76.json
    
    Signed-off-by: Stephane Eranian <eranian@gmail.com>

--- lib/pfmlib_arm_armv8.c
+++ lib/pfmlib_arm_armv8.c
@@ -36,6 +36,7 @@
 #include "events/arm_cortex_a57_events.h"    /* A57 event tables */
 #include "events/arm_cortex_a53_events.h"    /* A53 event tables */
 #include "events/arm_cortex_a55_events.h"    /* A53 event tables */
+#include "events/arm_cortex_a76_events.h"    /* A76 event tables */
 #include "events/arm_xgene_events.h"         /* Applied Micro X-Gene tables */
 #include "events/arm_cavium_tx2_events.h"    	/* Marvell ThunderX2 tables */
 #include "events/arm_fujitsu_a64fx_events.h"	/* Fujitsu A64FX PMU tables */
@@ -134,6 +135,22 @@ pfm_arm_detect_cortex_a55(void *this)
 
 	if ((pfm_arm_cfg.implementer == 0x41) && /* ARM */
 		(pfm_arm_cfg.part == 0xd05)) { /* Cortex A55 */
+			return PFM_SUCCESS;
+	}
+	return PFM_ERR_NOTSUPP;
+}
+
+static int
+pfm_arm_detect_cortex_a76(void *this)
+{
+	int ret;
+
+	ret = pfm_arm_detect(this);
+	if (ret != PFM_SUCCESS)
+		return PFM_ERR_NOTSUPP;
+
+	if ((pfm_arm_cfg.implementer == 0x41) && /* ARM */
+		(pfm_arm_cfg.part == 0xd0b)) { /* Cortex A76 */
 			return PFM_SUCCESS;
 	}
 	return PFM_ERR_NOTSUPP;
@@ -297,6 +314,33 @@ pfmlib_pmu_t arm_cortex_a55_support={
 	.pe			= arm_cortex_a55_pe,
 
 	.pmu_detect		= pfm_arm_detect_cortex_a55,
+	.max_encoding		= 1,
+	.num_cntrs		= 6,
+
+	.get_event_encoding[PFM_OS_NONE] = pfm_arm_get_encoding,
+	 PFMLIB_ENCODE_PERF(pfm_arm_get_perf_encoding),
+	.get_event_first	= pfm_arm_get_event_first,
+	.get_event_next		= pfm_arm_get_event_next,
+	.event_is_valid		= pfm_arm_event_is_valid,
+	.validate_table		= pfm_arm_validate_table,
+	.get_event_info		= pfm_arm_get_event_info,
+	.get_event_attr_info	= pfm_arm_get_event_attr_info,
+	 PFMLIB_VALID_PERF_PATTRS(pfm_arm_perf_validate_pattrs),
+	.get_event_nattrs	= pfm_arm_get_event_nattrs,
+};
+
+/* ARM Cortex A76 support */
+pfmlib_pmu_t arm_cortex_a76_support={
+	.desc			= "ARM Cortex A76",
+	.name			= "arm_ac76",
+	.perf_name		= "armv8_cortex_a76",
+	.pmu			= PFM_PMU_ARM_CORTEX_A76,
+	.pme_count		= LIBPFM_ARRAY_SIZE(arm_cortex_a76_pe),
+	.type			= PFM_PMU_TYPE_CORE,
+	.supported_plm          = ARMV8_PLM,
+	.pe			= arm_cortex_a76_pe,
+
+	.pmu_detect		= pfm_arm_detect_cortex_a76,
 	.max_encoding		= 1,
 	.num_cntrs		= 6,
 
